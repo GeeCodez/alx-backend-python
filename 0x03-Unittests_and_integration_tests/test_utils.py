e86c4cfe33cfb0195024dda78d1846b6dcb40689
Gee Codez <godsway702@gmail.com>
2025-11-13 22:38:37 +0000

    writing test cases for the exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,3 +28,18 @@ class TestAccessNestedMap(unittest.TestCase):
         map and a sequence of keys representing the path to the value.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected, f"{nested_map} following a sequence {path} must return {expected}")
+
+    #testing the exceptions using the same parameterized.expand method
+    @parameterized.expand([
+        ({}, ('a',)),
+        ({'a': 1}, ('a', 'b')),
+    ])
+    def test_access_nested_map_exception(self, nested_map: dict, path: tuple) -> None:
+        """
+        Test that access_nested_map raises KeyError for invalid paths
+        in nested maps.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+        
