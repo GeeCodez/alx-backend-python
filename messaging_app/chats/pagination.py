e0846c4d4bf2a5122913775fa5b0039729adfd67
Gee Codez <godsway702@gmail.com>
2025-11-23 01:02:20 +0000

    updating pagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 class MessagePagination(PageNumberPagination):
+    p=page.paginator.count
     page_size=20
     page_size_query_param='page_size'
     max_page_size=100
